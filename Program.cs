e1ca4f5377b7c51f5a9452b2ec348bb4673e9a3e
J_M_Lutra <jmlutra@gmail.com>
2023-08-20 21:01:34 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace JM_DiscordBlacklist
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
