4e0aefaa7b76641edab98feaa3b5e4c242567ba0
J_M_Lutra <jmlutra@gmail.com>
2023-08-21 17:01:37 +0200

    Add Blacklist frame

---
+++ LinkBlacklist/Blacklist.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JM_DiscordBlacklist.LinkBlacklist
+{
+    internal class Blacklist
+    {
+        private static List<Link> links = new();
+
+        public Blacklist()
+        { }
+
+        public static void AddLink(string topic, string url)
+        {
+            links.Add(new Link(topic, url));
+        }
+
+        public static Link SearchForLink(string potLink)
+        {
+            return links.Find(x => x.URL.Equals(potLink));
+        }
+
+    }
+}
---
+++ LinkBlacklist/Link.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JM_DiscordBlacklist.LinkBlacklist
+{
+    internal class Link
+    {
+        public string Topic { get; }
+        public string URL { get; }
+        public Link(string topic, string url)
+        {
+            Topic = topic;
+            URL = url;
+        }
+    }
+}
