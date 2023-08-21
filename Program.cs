5370d27bcb6272c2f3f9353cd04b290a165f70fb
J_M_Lutra <jmlutra@gmail.com>
2023-08-21 17:01:26 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,10 +1,13 @@
-﻿namespace JM_DiscordBlacklist
+﻿using Newtonsoft.Json.Linq;
+
+namespace JM_DiscordBlacklist
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+            Bot bot = new Bot(args[1]);
+            bot.RunAsync().Wait();
         }
     }
 }
